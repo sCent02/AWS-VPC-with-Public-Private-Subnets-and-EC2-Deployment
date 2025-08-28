ac1dce80c4ecc34295db870d0163e84b331d78ae
unknown <vince.52.va@gmail.com>
2025-08-28 08:57:43 +0800

    Delete Key_Nmae for security purposes

--- variables.tf
+++ variables.tf
@@ -20,5 +20,5 @@ variable "instance_type" {
 
 variable "key_name" {
   description = "Existing AWS key pair name for SSH"
-  default     = "T-Windows"  # Put your own .pem file
+  default     = ""  # Put your own .pem file
 }
